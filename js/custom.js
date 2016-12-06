1f719db6c6528446b5b004a6108342e22c899bd8
Muhammed Mishal <muhdmishal@gmail.com>
2016-12-06 21:42:49 +0530

    Title for links

--- js/custom.js
+++ js/custom.js
@@ -3,4 +3,11 @@ function processForm() {
 
     return true; //Submit the form now
     //Alternatively you can return false to NOT submit the form.
-  }
+}
+window.onload = function() {
+  var facebook = document.getElementsByClassName("facebook");
+  facebook[0].setAttribute("href", "https://www.facebook.com/sharer/sharer.php?u="+window.location.href);
+  facebook[0].setAttribute("title", "Share this page on Facebook");
+  var twitter = document.getElementsByClassName("twitter");
+  twitter[0].setAttribute("title", "Tweet this page.");
+};
